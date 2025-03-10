491602c661551635b7797bcabe1636f12843670f
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-03-10 22:12:44 +0300

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { App } from '@/pages/App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <App />
+)
+
---
+++ src/pages/App/ui/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+export const App = () => (
+  <div className="app-container">
+    <h1 data-testid='AppTitle'>Gift Shop</h1>
+  </div>
+);
