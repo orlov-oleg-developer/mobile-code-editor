f99e3288e290e613385bd64b18911be120910bf8
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-03-11 21:32:10 +0300

    feat: смена языка

--- src/pages/App/ui/App.tsx
+++ src/pages/App/ui/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { EditorView, keymap, highlightActiveLine } from "@codemirror/view";
 import { EditorState } from "@codemirror/state";
 import { defaultKeymap, history } from "@codemirror/commands";
@@ -9,16 +9,20 @@ import styles from "./App.module.css";
 export const App = () => {
   const editorRef = useRef(null);
   const editorViewRef = useRef<EditorView | null>(null);
+  const [language, setLanguage] = useState<"js" | "ts">("js");
+
 
   useEffect(() => {
     if (!editorRef.current) return;
+
+    const languageExtension = javascript({ jsx: true, typescript: language === "ts" });
 
     const state = EditorState.create({
       doc: "console.log('Hello, world!');",
       extensions: [
         keymap.of(defaultKeymap), // Горячие клавиши
         history(), // История изменений
-        javascript(), // Подсветка синтаксиса JS
+        languageExtension, // Подсветка синтаксиса JS
         highlightActiveLine(), // Подсветка активной строки
         oneDark, // Темная тема
         EditorView.lineWrapping // Перенос строк
@@ -28,15 +32,22 @@ export const App = () => {
     editorViewRef.current = new EditorView({
       state: state,
       parent: editorRef.current,
-      // extensions: [basicSetup, javascript()]
     })
 
     return () => editorViewRef.current?.destroy();
-  }, []);
+  }, [language]);
+
+  const handleLanguageChange = (newLanguage: "js" | "ts") => {
+    setLanguage(newLanguage);
+  };
 
   return (
     <div className={styles.container}>
       <h1>JS Code Editor</h1>
+      <div>
+        <button onClick={() => handleLanguageChange("js")} className={language === 'js' ? styles.selected : undefined} >JavaScript</button>
+        <button onClick={() => handleLanguageChange("ts")} className={language === 'ts' ? styles.selected : undefined} >TypeScript</button>
+      </div>
       <div ref={editorRef} className={styles.editor} />
     </div>
   );
