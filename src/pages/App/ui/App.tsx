40a9916e86bb948303b9f88090dddacfd41ec9da
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-03-11 19:37:29 +0300

    feat: base setup

--- src/pages/App/ui/App.tsx
+++ src/pages/App/ui/App.tsx
@@ -1,7 +1,33 @@
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import { EditorView, keymap } from "@codemirror/view";
+import { EditorState } from "@codemirror/state";
+import styles from "./App.module.css";
+import { defaultKeymap } from "@codemirror/commands"
 
-export const App = () => (
-  <div className="app-container">
-    <h1 data-testid='AppTitle'>Gift Shop</h1>
-  </div>
-);
+export const App = () => {
+  const editorRef = useRef(null);
+  const editorViewRef = useRef<EditorView | null>(null);
+
+  useEffect(() => {
+    if (!editorRef.current) return;
+
+    const state = EditorState.create({
+      doc: "Hello World",
+      extensions: [keymap.of(defaultKeymap)]
+    })
+
+    editorViewRef.current = new EditorView({
+      state: state,
+      parent: editorRef.current,
+    })
+
+    return () => editorViewRef.current?.destroy();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1>JS Code Editor</h1>
+      <div ref={editorRef} className={styles.editor} />
+    </div>
+  );
+};
