b04d6310a664b831b13cab211fd2d68495c5beb0
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-03-11 20:05:10 +0300

    feat: gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mobile-code-editor/',
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
